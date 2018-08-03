e37178fcb9ede8baf4568417fa371f4104b0c6ad
PubuduJayaweera <40852358+PubuduJayaweera@users.noreply.github.com>
2018-08-03 16:28:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const http = require('http');
-
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 let server = http.createServer((request, response) => {
     const {headers, method, url} = request;
     let body = [];
@@ -20,3 +20,38 @@ let server = http.createServer((request, response) => {
         // do whatever we need to in order to respond to this request.
     });
 }).listen(8080); // Activates this server, listening on port 8080.
+//}).listen(433); // testing
+//discord
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.generateInvite(["ADMINISTRATOR"]).then(link => {
+        console.log(link);
+//        client.channels.get('449616073642475531').send('ESP8266 Online!');
+//        var x = Date();
+//        client.channels.get('449616073642475531').send(x);
+    });
+});
+client.on('message', msg => {
+    const sp = msg.content.toString().split(" ");
+    if (msg.content === ".ping") {
+        msg.channel.send(client.ping + " ms");
+//        msg.delete();
+    }
+    else if(sp[0] === ".test"){
+        msg.channel.send("Sending a message to " + sp[1]);
+        var ip = sp[1];
+        const xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (this.readyState === 4 && this.status === 200) {
+                    var output = this.responseText;
+                    msg.channel.send("Output :- " + output);
+                }
+            }
+            xmlhttp.open("POST", "http://" + ip + ":433/logger/logger.php", true);
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlhttp.send("msg=ping");
+    }
+});
+client.login('NDQ4OTE4ODI5NzYzMTMzNDQw.DehVRA.arwJxX73rnkln_9GrbhhZZ6RThk');
